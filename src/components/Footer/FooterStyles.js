39692e26e6296b12093b8efc38b6438c6ba14878
Adkins1 <Adkins11111@gmail.com>
2022-02-18 00:26:20 +0100

    few landscape orientation fixes

--- src/components/Footer/FooterStyles.js
+++ src/components/Footer/FooterStyles.js
@@ -1,107 +1,110 @@
-import styled from "styled-components";
-import { color, maxDevice } from "../variables";
-import { Link as LinkR } from "react-router-dom";
+import styled from "styled-components"
+import { color, maxDevice } from "../variables"
+import { Link as LinkR } from "react-router-dom"
 
 export const Container = styled.div`
-    width: 100%;
-    height: 60vh;
-    background: ${color.darkBlue};
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 60vh;
+  background: ${color.darkBlue};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  @media (orientation: landscape) {
+    height: 100vh;
+  }
 `
 export const Wrapper = styled.div`
-    width: 80%;
-    height: 80%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
+  width: 80%;
+  height: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
 `
 export const Links = styled.ul`
-    height: 100%;
-    width: 40%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    @media screen and (${maxDevice.tablet}) {
-        width: 100%;
-        align-items: center;
-    }
+  height: 100%;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  @media screen and (${maxDevice.tablet}) {
+    width: 100%;
+    align-items: center;
+  }
 `
 export const Link = styled(LinkR)`
-    text-decoration: none;
-    font-size: 2rem;
-    color: ${color.white};
-    padding: 5px;
-    transition: .2s;
-    :hover{
-        font-size: 2.5rem;
-        transition: .2s;
-        background: ${color.darkBlueHover}
-   }
-   @media screen and (${maxDevice.tablet}) {
-        font-size: 1rem;
+  text-decoration: none;
+  font-size: 2rem;
+  color: ${color.white};
+  padding: 5px;
+  transition: 0.2s;
+  :hover {
+    font-size: 2.5rem;
+    transition: 0.2s;
+    background: ${color.darkBlueHover};
+  }
+  @media screen and (${maxDevice.tablet}) {
+    font-size: 1rem;
 
-        :hover{
-            font-size: 1.5rem;
-        }
+    :hover {
+      font-size: 1.5rem;
     }
+  }
 `
 export const Media = styled.div`
-    width: 40%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    @media screen and (${maxDevice.tablet}) {
-        width: 100%;
-        justify-content: center;
-    }
+  width: 40%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  @media screen and (${maxDevice.tablet}) {
+    width: 100%;
+    justify-content: center;
+  }
 `
 export const Row = styled.div`
-    min-height: 30%;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 50px;
-    @media screen and (${maxDevice.tablet}) {
-        flex-direction: column;
-        gap: 0;
-    }
+  min-height: 30%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 50px;
+  @media screen and (${maxDevice.tablet}) {
+    flex-direction: column;
+    gap: 0;
+  }
 `
 export const Header = styled.div`
-    font-size: 2rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    font-weight: 900;
-    color: ${color.white};
-    @media screen and (${maxDevice.tablet}) {
-        font-size: 1rem;
-    }
+  font-size: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-weight: 900;
+  color: ${color.white};
+  @media screen and (${maxDevice.tablet}) {
+    font-size: 1rem;
+  }
 `
 export const MediaLink = styled.a`
-    font-size: 3rem;
-    padding: 10px;
-    text-decoration: none;
-    color: ${color.white};
-    transition: .2s;
-    :hover{
-        transition: .2s;
-        background: ${color.darkBlueHover}
-    }
+  font-size: 3rem;
+  padding: 10px;
+  text-decoration: none;
+  color: ${color.white};
+  transition: 0.2s;
+  :hover {
+    transition: 0.2s;
+    background: ${color.darkBlueHover};
+  }
 `
 export const Logo = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 `
 export const LogoLink = styled(LinkR)`
-    width: 40%;
-    height: 100%;
-    transition: .2s;
-    :hover{
-        background: ${color.darkBlueHover};
-    }
+  width: 40%;
+  height: 100%;
+  transition: 0.2s;
+  :hover {
+    background: ${color.darkBlueHover};
+  }
 `
--- src/components/Navbar/NavbarStyles.js
+++ src/components/Navbar/NavbarStyles.js
@@ -1,98 +1,103 @@
-import styled from "styled-components";
-import { Link as LinkR } from "react-router-dom";
-import { color, maxDevice } from '../variables'
+import styled from "styled-components"
+import { Link as LinkR } from "react-router-dom"
+import { color, maxDevice } from "../variables"
 
 export const Container = styled.div`
-    position: relative;
-    padding: 0 10px 0 10px;
-    width: 100%;
-    height: ${({ showMenu }) => showMenu ? "100vh" : "8vh"};
-    flex-direction: ${({ showMenu }) => showMenu ? "column" : "row"};
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: nowrap;
-    background: ${color.darkBlue};
-    @media screen and (${maxDevice.tablet}) {
-        align-items: center;
-    }
+  position: relative;
+  padding: 0 10px 0 10px;
+  width: 100%;
+  height: ${({ showMenu }) => (showMenu ? "100vh" : "8vh")};
+  flex-direction: ${({ showMenu }) => (showMenu ? "column" : "row")};
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: nowrap;
+  background: ${color.darkBlue};
+  @media screen and (${maxDevice.tablet}) {
+    align-items: center;
+  }
+  @media (orientation: landscape) {
+    height: ${({ showMenu }) => (showMenu ? "100vh" : "16vh")};
+  }
 `
 export const LogoLink = styled(LinkR)`
-    width: 10%;
-    height: 100%;
-    transition: .2s;
-    :hover{
-        background: ${color.darkBlueHover};
-    }
-    @media screen and (${maxDevice.tablet}) {
-        width: 30%;
-        height: ${({ showMenu }) => showMenu ? "40%" : "100%"};
-    }
+  width: 10%;
+  height: 100%;
+  transition: 0.2s;
+  :hover {
+    background: ${color.darkBlueHover};
+  }
+  @media screen and (${maxDevice.tablet}) {
+    width: 30%;
+    height: ${({ showMenu }) => (showMenu ? "40%" : "100%")};
+  }
+  @media (orientation: landscape) {
+    width: 20%;
+  }
 `
 export const Logo = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
 `
 export const Links = styled.div`
-    width: 90%;
-    height: 100%;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+  width: 90%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  @media screen and (${maxDevice.tablet}) {
+    width: 100%;
+    flex-direction: column;
+  }
+  a {
     @media screen and (${maxDevice.tablet}) {
-        width: 100%;
-        flex-direction: column;
+      display: ${({ showMenu }) => (showMenu ? "flex" : "none")};
+      width: 100%;
+      font-size: 2rem;
     }
-    a{
-        @media screen and (${maxDevice.tablet}) {
-            display: ${({ showMenu }) => showMenu ? "flex" : "none"};
-            width: 100%;
-            font-size: 2rem;
-        }
-    }
+  }
 `
 export const Link = styled(LinkR)`
-    display: flex;
-    justify-content: center;
-    gap: 10px;
-    align-items: center;
-    font-size: 1.4rem;
-    color: ${color.white};
-    text-decoration: none;
-    width: 20%;
-    height: 100%;
-    transition: .2s;
-    :hover{
-        background: ${color.darkBlueHover};
-    }
-    svg{
-        font-size: 1.2em;
-    }
-    @media screen and (${maxDevice.laptop}) {
-        font-size: 1rem;
-        width: 30%;
-    }
-    
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  align-items: center;
+  font-size: 1.4rem;
+  color: ${color.white};
+  text-decoration: none;
+  width: 20%;
+  height: 100%;
+  transition: 0.2s;
+  :hover {
+    background: ${color.darkBlueHover};
+  }
+  svg {
+    font-size: 1.2em;
+  }
+  @media screen and (${maxDevice.laptop}) {
+    font-size: 1rem;
+    width: 30%;
+  }
 `
 export const Hamburger = styled.div`
-    display: none;
+  display: none;
 
-    @media screen and (${maxDevice.tablet}) {
-        display: block;
-        position: absolute;
-        top: 30px;
-        right: 30px;
-        font-size: 1.8rem;
-        cursor: pointer;
-        transition: .2s;
-        color: ${color.white};
-        :hover{
-            transform: scale(110%);
-            transition: .2s;
-        }
-        @media screen and (${maxDevice.laptop}) {
-            top: 15px;
-            right: 15px;
-        }
+  @media screen and (${maxDevice.tablet}) {
+    display: block;
+    position: absolute;
+    top: 30px;
+    right: 30px;
+    font-size: 1.8rem;
+    cursor: pointer;
+    transition: 0.2s;
+    color: ${color.white};
+    :hover {
+      transform: scale(110%);
+      transition: 0.2s;
     }
+    @media screen and (${maxDevice.laptop}) {
+      top: 15px;
+      right: 15px;
+    }
+  }
 `
